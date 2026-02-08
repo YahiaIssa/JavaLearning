396cbf3c3b502bc55b5b2fd84085736b5a5fbd11
yahia <yramadan200105@gmail.com>
2026-02-08 16:58:09 +0300

    do while

--- src/javacourse/Javacourse.java
+++ src/javacourse/Javacourse.java
@@ -5,13 +5,13 @@ public class Javacourse {
 
 
     public static void main(String[] args) {
-int x=1;
-while (x<10){
-    System.out.println("i");
+int x=0;
+do {
+    System.out.println(x);
     x++;
-
-
 }
+while (x<1);
+
 
 
     }
