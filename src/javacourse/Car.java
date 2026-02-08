18771498bcb61d2f5f2c96b6020765ba78cd0c9c
yahia <yramadan200105@gmail.com>
2026-02-08 22:40:32 +0300

    multidimensional array

--- src/javacourse/Car.java
+++ src/javacourse/Car.java
@@ -15,46 +15,45 @@ object is a copy from the class
 public class Car {
 
 
+    public int model;
+    public float price;
 
-//    public int model;
-//    public float price;
-//
-//    public short max_speed;
-//
-//
-//    public void Speed(int s){
-//
-//        System.out.println("Speed :"+s);
-//    }
-//    public int speed;
-//
-//    public Car(int s){
-//
-//this.speed=s;
-//
-//    }
-//    public void setSpeed(int s){
-//        this.speed=s;
-//
-//
-//        switch (s){
-//            case 100:
-//                System.out.println("be carful");
-//                break;
-//            case 120:
-//                System.out.println("slow down");
-//                break;
-//            case 50:
-//                System.out.println("good driver");
-//            default:
-//                System.out.println("okay");
+    public short max_speed;
 
 
+    public void Speed(int s) {
 
-
-
-      //  }
+        System.out.println("Speed :" + s);
     }
+
+    public int speed;
+
+    public Car(int s) {
+
+        this.speed = s;
+
+    }
+
+    public void setSpeed(int s) {
+        this.speed = s;
+
+
+        switch (s) {
+            case 100:
+                System.out.println("be carful");
+                break;
+            case 120:
+                System.out.println("slow down");
+                break;
+            case 50:
+                System.out.println("good driver");
+            default:
+                System.out.println("okay");
+
+
+        }
+    }
+}
 
 
 
--- src/javacourse/Javacourse.java
+++ src/javacourse/Javacourse.java
@@ -1,34 +1,73 @@
 package javacourse;
 
 
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Javacourse {
 
+//    public static int[] createArray(int size)
+//    {
+//    int c []=new int[size];
+//    return c;
+//    }
 
     public static void main(String[] args) {
+//
+//        int values[]=createArray(7);
+//        int x []=createArray(22);
+//
+//        System.out.println(values.length);
+//        System.out.println(x.length);
+//
+//
+//    int z[] = {1,2,3,4,5,6,7,8,9};
+//        for (int i = 0; i <z.length ; i++) {
+//            System.out.println(z[i]);
+//        }
+//        int num2[]={1,2,3,4,5,6,7,8};
+//        System.out.println(num2.length);
+//        String nam="yahia66";
+//        int num1[]={1,2,3,4,5};
+//        System.out.println(nam.length());
+//        System.out.println(num1.length);
+//        String name []={"yahia","issa","ramadan"};
+//        for (int i = 0; i <name.length ; i++) {
+//            System.out.print(" ");
+//            System.out.print(name[i]);
+//        }
+//        int values[] = {3, 567, 56};
+//        for (int v : values) {
+//            System.out.println(v);
+//        }
+//        System.out.println(values[0]);
+        int values[] = new int[10];
+        Car y[] = new Car[10];
 
-    int z[] = {1,2,3,4,5,6,7,8,9};
-        for (int i = 0; i <z.length ; i++) {
-            System.out.println(z[i]);
+        int[][] x = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        System.out.println(x[2][0]);
+
+        int z[][] = new int[2][4];
+
+        z[0][0] = 1;
+        z[0][1] = 2;
+        z[0][2] = 3;
+        z[0][3] = 4;
+
+        z[1][0] = 1;
+        z[1][1] = 2;
+        z[1][2] = 3;
+        z[1][3] = 4;
+
+        for(int i=0;i< z.length;i++){
+            int[] r=z[i];
+
+            System.out.println(Arrays.toString(r));
         }
 
 
-        int num2[]={1,2,3,4,5,6,7,8};
-        System.out.println(num2.length);
 
-
-        String nam="yahia66";
-        int num1[]={1,2,3,4,5};
-        System.out.println(nam.length());
-        System.out.println(num1.length);
-
-        String name []={"yahia","issa","ramadan"};
-        for (int i = 0; i <name.length ; i++) {
-            System.out.print(" ");
-            System.out.print(name[i]);
-
-        }
     }
-}
 
+}
