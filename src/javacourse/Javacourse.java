efff60d7d3415fa4bb4a75993bdb531571769003
yahia <yramadan200105@gmail.com>
2026-02-08 18:26:32 +0300

    Data type

--- src/javacourse/Javacourse.java
+++ src/javacourse/Javacourse.java
@@ -1,19 +1,41 @@
 package javacourse;
 
 
+import java.util.Locale;
+
 public class Javacourse {
 
 
     public static void main(String[] args) {
-int x=0;
-do {
-    System.out.println(x);
-    x++;
-}
-while (x<1);
+//Data type
+        char z='c';
+        System.out.println(z);
+        String x="Data type course";
+        System.out.println("x");
+//        char c=x.charAt(0);
+//        System.out.println(c);
+        int l =x.length();
+        System.out.println(l);
+        for (int i = 0; i <x.length() ; i++) {
+            char c=x.charAt(i);
+            System.out.println(c);
+        }
+        String value=new String("YaHiA");
+        System.out.println(value);
 
+        String name="Yahia";
+        String nname=name.toUpperCase();
+        System.out.println(nname);
+        String mname=name.toLowerCase();
+        System.out.println(mname);
 
+        String site="www.google.com";
+        String xaite="www.googlE.com";
 
+      if(site.equalsIgnoreCase(xaite))
+          System.out.println("ok");
+      else
+          System.out.println("no");
     }
 }
 
